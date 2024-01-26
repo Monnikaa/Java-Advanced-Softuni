8442e112c5ba2654720113de59d41844f198a7ed
Monika <155815244+Monnikaa@users.noreply.github.com>
2024-01-26 19:36:38 +0000

    Update MatchingBrackets_4.java

--- Advanced/_1StackAndQueue/MatchingBrackets_4.java
+++ Advanced/_1StackAndQueue/MatchingBrackets_4.java
@@ -1,4 +1,27 @@
 package _1StackAndQueue;
 
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class MatchingBrackets_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        //1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char current = input.charAt(i);
+
+            if ('(' == current) {
+                stack.push(i);
+            } else if (')' == current) {
+                int startIndex = stack.pop();
+                String match = input.substring(startIndex, i + 1);
+                System.out.println(match);
+            }
+        }
+
+    }
 }
