060a29c6bff2b70ae313bd3aa8a123d665ddb6b7
Monika <monika.blagoeva77@gmail.com>
2024-02-22 12:05:09 +0000

    first commit second commit

--- Advanced/_1StackAndQueue/HotPotato_6.java
+++
@@ -1,9 +0,0 @@
-package _1StackAndQueue;
-
-import java.util.Scanner;
-
-public class HotPotato_6 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-    }
-}
